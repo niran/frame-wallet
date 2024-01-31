cb66bb44848a7c9e397404673f327d596f0eb1f7
Niran Babalola <niran@niran.org>
2024-01-31 20:20:32 +0000

    Create inital frame

---
+++ app/[chainId]/[calldata]/page.js
@@ -0,0 +1,29 @@
+
+const imageUrl = "/images/robot-check.png";
+
+export default function Page({ params }) {
+  return (
+    <html>
+      <head>
+        <meta property="og:title" content="Frame Wallet Transaction" />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={imageUrl} />
+        <meta property="fc:frame:button:1" content="Sign Transaction" />
+      </head>
+      <body>
+        <img src={imageUrl} width="800" />
+        <table>
+          <tr>
+            <td>Chain ID</td>
+            <td>{params.chainId}</td>
+          </tr>
+          <tr>
+            <td>Calldata</td>
+            <td>0x{params.calldata}</td>
+          </tr>
+        </table>
+      </body>
+    </html>
+  )
+}
