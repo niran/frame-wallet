38f0842a8c874b8d374a410256a2b20ded476b94
Niran Babalola <niran@niran.org>
2024-02-04 04:02:31 +0000

    Create a synthetic FrameUserSignature for testing

--- contracts/test/FrameWallet.t.sol
+++ contracts/test/FrameWallet.t.sol
@@ -5,7 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {EntryPoint} from "account-abstraction/core/EntryPoint.sol";
 import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-
+import "frame-verifier/Encoder.sol";
 
 import {FrameWallet} from "../src/FrameWallet.sol";
 import {FrameWalletFactory} from "../src/FrameWalletFactory.sol";
@@ -41,18 +41,17 @@ contract FrameWalletTest is Test {
         ));
 
         /*
-        "trustedData":{"messageBytes":"0ac101080d10df920e18c38cc22e20018201b0010a8f0168747470733a2f2f6672616d652d77616c6c65742e76657263656c2e6170702f383435332f3738396364623236616266653864303130393334396237366665623966363736646238333765396631616133326264666135343366363265643031343636613030633234313039303737653138633562626539393432336231386631343930323030356165613066343910011a1a08df920e121400000000000000000000000000000000000000011214f053e34a7b16baf4d8e2691bb786a57ca4c6fdd91801224070de501ed8538475dd9fd51e73e82d944edf1f928feb4cec9715ed0a3e8bd518b076203b995beddb4116822f8e65899a391b7d18b0ddf4acdf298e48f40f410e2801322031351506585341467af8e18295bbd3eea2d5ea942edaf612f915f8e9cf639419"}
-
+        "trustedData":{"messageBytes":"0ac101080d10df920e18afc9c22e20018201b0010a8f0168747470733a2f2f6672616d652d77616c6c65742e76657263656c2e6170702f383435333a3738396364623236616266653864303130393334396237366665623966363736646238333765396631616133326264666135343366363265643031343636613030633234313039303737653138633562626539393432336231386631343930323030356165613066343910011a1a08df920e121400000000000000000000000000000000000000011214f0dcc3983c1e6c5adc3d5281c12dee3e0d0e860d18012240ed75d516b6bc74e71804b153fede88800e70c0828a567589b790854e4f4df8663c1b7bb454f5fa44486259d0f10e9dc8ce67834fb263c8a78fb17be51950420d2801322031351506585341467af8e18295bbd3eea2d5ea942edaf612f915f8e9cf639419"}
         {
             "valid": true,
             "message": {
                 "data": {
                 "type": "MESSAGE_TYPE_FRAME_ACTION",
                 "fid": 231775,
-                "timestamp": 97551939,
+                "timestamp": 97559727,
                 "network": "FARCASTER_NETWORK_MAINNET",
                 "frameActionBody": {
-                    "url": "aHR0cHM6Ly9mcmFtZS13YWxsZXQudmVyY2VsLmFwcC84NDUzLzc4OWNkYjI2YWJmZThkMDEwOTM0OWI3NmZlYjlmNjc2ZGI4MzdlOWYxYWEzMmJkZmE1NDNmNjJlZDAxNDY2YTAwYzI0MTA5MDc3ZTE4YzViYmU5OTQyM2IxOGYxNDkwMjAwNWFlYTBmNDk=",
+                    "url": "aHR0cHM6Ly9mcmFtZS13YWxsZXQudmVyY2VsLmFwcC84NDUzOjc4OWNkYjI2YWJmZThkMDEwOTM0OWI3NmZlYjlmNjc2ZGI4MzdlOWYxYWEzMmJkZmE1NDNmNjJlZDAxNDY2YTAwYzI0MTA5MDc3ZTE4YzViYmU5OTQyM2IxOGYxNDkwMjAwNWFlYTBmNDk=",
                     "buttonIndex": 1,
                     "castId": {
                     "fid": 231775,
@@ -61,14 +60,33 @@ contract FrameWalletTest is Test {
                     "inputText": ""
                 }
                 },
-                "hash": "0xf053e34a7b16baf4d8e2691bb786a57ca4c6fdd9",
+                "hash": "0xf0dcc3983c1e6c5adc3d5281c12dee3e0d0e860d",
                 "hashScheme": "HASH_SCHEME_BLAKE3",
-                "signature": "cN5QHthThHXdn9Uec+gtlE7fH5KP60zslxXtCj6L1RiwdiA7mVvt20EWgi+OZYmaORt9GLDd9KzfKY5I9A9BDg==",
+                "signature": "7XXVFra8dOcYBLFT/t6IgA5wwIKKVnWJt5CFTk9N+GY8G3u0VPX6REhiWdDxDp3IzmeDT7JjyKePsXvlGVBCDQ==",
                 "signatureScheme": "SIGNATURE_SCHEME_ED25519",
                 "signer": "0x31351506585341467af8e18295bbd3eea2d5ea942edaf612f915f8e9cf639419"
             }
-        }
+            }
         */
+
+        MessageData memory md = MessageData({
+            type_: MessageType.MESSAGE_TYPE_FRAME_ACTION,
+            fid: 231775,
+            timestamp: 97559727,
+            network: FarcasterNetwork.FARCASTER_NETWORK_MAINNET,
+            frame_action_body: FrameActionBody({
+                url: "https://frame-wallet.vercel.app/8453:789cdb26abfe8d0109349b76feb9f676db837e9f1aa32bdfa543f62ed01466a00c24109077e18c5bbe99423b18f14902005aea0f49",
+                button_index: 1,
+                cast_id: CastId({fid: 231775, hash: 0x0000000000000000000000000000000000000001})
+            })
+        });
+
+        FrameWallet.FrameUserOpSignature memory frameSig = FrameWallet.FrameUserOpSignature({
+            md: md,
+            ed25519sig: hex"ed75d516b6bc74e71804b153fede88800e70c0828a567589b790854e4f4df8663c1b7bb454f5fa44486259d0f10e9dc8ce67834fb263c8a78fb17be51950420d",
+            urlPrefix: "https://frame-wallet.vercel.app/",
+            compressedCallData: hex"789cdb26abfe8d0109349b76feb9f676db837e9f1aa32bdfa543f62ed01466a00c24109077e18c5bbe99423b18f14902005aea0f49"
+        });
 
         // TODO: Goal is to verify that InflateLib is working correctly ASAP
         UserOperation memory userOp = UserOperation({
@@ -79,10 +97,10 @@ contract FrameWalletTest is Test {
             callGasLimit: 0,
             verificationGasLimit: 0,
             preVerificationGas: 0,
-            maxFeePerGas: 0,
+            maxFeePerGas: 1 gwei,
             maxPriorityFeePerGas: 0,
             paymasterAndData: hex"",
-            signature: hex""
+            signature: abi.encode(frameSig)
         });
         console.logBytes(outerCallData);
         console.log(Base64.encode(outerCallData));
