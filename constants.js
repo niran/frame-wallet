8046451bd827ec46fba29432000162baefb067fd
Niran Babalola <niran@niran.org>
2024-02-09 20:24:09 +0000

    Use the Pimlico RPC for 4337 transactions

--- constants.js
+++ constants.js
@@ -2,6 +2,6 @@ export const BASE_URL = "https://frame-wallet.vercel.app";
 export const CHAIN_ID = 8453;
 export const HUB_URL = process.env['HUB_URL'] || "nemes.farcaster.xyz:2283";
 // export const RPC_URL = "https://mainnet.base.org";
-export const RPC_URL = "https://sepolia.base.org";
+export const RPC_URL = "https://api.pimlico.io/v1/base-sepolia/rpc?apikey=" + (process.env.PIMLICO_API_KEY | '');
 export const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 export const IMAGE_URL = "/images/robot-check.png";
