3eaf2d72550d83c139e05c73c08a64c02d85534e
Niran Babalola <niran@niran.org>
2024-02-09 02:41:30 +0000

    Remove babel config which breaks vercel, and attempt to replace it with config from the next.js docs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: [
-    "/node_modules/",
     "<rootDir>/contracts",
-  ]
+  ],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
