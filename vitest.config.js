d5f634ea24e05809885b422b5b7f1f8a4084cd5d
Niran Babalola <niran@niran.org>
2024-02-10 22:35:16 +0000

    Switch from jest to vitest because jest doesn't work

---
+++ vitest.config.js
@@ -0,0 +1,10 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+ 
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'contracts/**'],
+  },
+})
