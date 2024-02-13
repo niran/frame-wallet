f475a4098d0151bb8d9ffa2b10c1d0cd71060a4a
Niran Babalola <niran@niran.org>
2024-02-13 04:44:47 +0000

    Remove custom font until we can get it to work

--- app/v1/[compressedPartialUserOp]/i/route.tsx
+++ app/v1/[compressedPartialUserOp]/i/route.tsx
@@ -6,6 +6,7 @@ import { RouteParams } from '../types';
 
 export async function GET(req: NextRequest, { params }: { params: RouteParams }) {
   const element = await htmlHandler({ params });
+  /*
   const fontUrl = new URL(
     '../../../../node_modules/@fontsource/roboto-mono/files/roboto-mono-latin-400-normal.woff',
     import.meta.url,
@@ -13,11 +14,12 @@ export async function GET(req: NextRequest, { params }: { params: RouteParams })
   console.log(import.meta.url);
   console.log(fontUrl);
   const robotoMono400 = fetch(fontUrl).then((res) => res.arrayBuffer());
+  */
 
   return new ImageResponse(element, {
     width: 1080,
     height: 566,
     emoji: 'noto',
-    fonts: [{ name: 'Roboto_Mono_400', data: await robotoMono400, weight: 400 }],
+    // fonts: [{ name: 'Roboto_Mono_400', data: await robotoMono400, weight: 400 }],
   });
 }
