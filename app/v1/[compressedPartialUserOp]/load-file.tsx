5cc1cb50b0f0073381c37d334f5dd8c1076a6c02
Niran Babalola <niran@niran.org>
2024-02-14 23:21:38 +0000

    Generate the path to loaded files without listing directories

--- app/v1/[compressedPartialUserOp]/load-file.tsx
+++ app/v1/[compressedPartialUserOp]/load-file.tsx
@@ -5,24 +5,10 @@ import * as path from 'path';
 
 async function getProjectRoot() {
   const metaURL = new URL(import.meta.url);
-  let lastPath = metaURL.pathname;
-  let rootPath = lastPath;
-
-  while (true) {
-    rootPath = path.join(lastPath, '..');
-    if (rootPath === lastPath) {
-      break;
-    }
-
-    const files = await fs.readdir(rootPath);
-    if (files.find(file => file === 'jsconfig.json' || file === 'tsconfig.json')) {
-      return rootPath;
-    }
-
-    lastPath = rootPath;
-  }
-
-  return rootPath;
+  const hierarchy = metaURL.pathname.split('/');
+  const appIndex = hierarchy.findIndex(dir => dir === 'app');
+  const rootHierarchy = hierarchy.slice(0, appIndex);
+  return path.join('/', ...rootHierarchy);
 }
 
 export async function loadImageURIFromFile(pathFromProjectRoot: string, mimeType: string) {
