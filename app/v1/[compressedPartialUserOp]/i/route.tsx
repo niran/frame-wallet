4aa2f24e501cdd6c2e3eaa8ce646d7aadfbeafc6
Niran Babalola <niran@niran.org>
2024-02-15 16:49:37 +0000

    Remove the custom font again since it won't load on Vercel

--- app/v1/[compressedPartialUserOp]/i/route.tsx
+++ app/v1/[compressedPartialUserOp]/i/route.tsx
@@ -7,12 +7,12 @@ import { loadFile } from '../load-file';
 
 export async function GET(req: NextRequest, { params }: { params: RouteParams }) {
   const element = await htmlHandler({ params });
-  const robotoMono400 = await loadFile('node_modules/@fontsource/roboto-mono/files/roboto-mono-latin-400-normal.woff');
+  // const robotoMono400 = await loadFile('node_modules/@fontsource/roboto-mono/files/roboto-mono-latin-400-normal.woff');
 
   return new ImageResponse(element, {
     width: 1080,
     height: 566,
     emoji: 'noto',
-    fonts: [{ name: 'Roboto_Mono_400', data: robotoMono400, weight: 400 }],
+    // fonts: [{ name: 'Roboto_Mono_400', data: robotoMono400, weight: 400 }],
   });
 }
