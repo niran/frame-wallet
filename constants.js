c4c38a1ff0eebc1adae59276be8cbc88494df94b
Niran Babalola <niran@niran.org>
2024-02-20 22:34:59 +0000

    Deploy Base Sepolia contracts pointing to 0xfwd.vercel.app

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://0xfw.vercel.app";
+export const BASE_URL = "https://0xfwd.vercel.app";
 export const HUB_URL = process.env['HUB_URL'] || "nemes.farcaster.xyz:2283";
 export const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 export const IMAGE_URL = "/images/robot-check.png";
